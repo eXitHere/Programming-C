4676a6d6991103d84af205852c0623d0f39513fc
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2020-05-05 22:20:30 +0700

    Rename

--- 0011.c
+++ 0011.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-    int arr[43] = {0},input,i,resulte = 10;
+    int arr[43] = {0},input,i,result = 10;
     for(i=0;i<10;i++){
         scanf("%d",&input);
         arr[input%42]++;
         if(arr[input%42]>1){
-            resulte--;
+            result--;
         }
     }
-    printf("%d",resulte);
+    printf("%d",result);
 }
